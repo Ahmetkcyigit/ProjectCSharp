9d6cc4f298a14f4842c7254b97ecf6475485f691
Ahmet Koçyiğit <kocyigittahmet@gmail.com>
2024-12-19 22:44:55 +0300

    19.12

--- PresentationLayer/FrmCategory.cs
+++ PresentationLayer/FrmCategory.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntitiyFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +16,53 @@ namespace PresentationLayer
 {
     public partial class FrmCategory : Form
     {
+        private readonly ICategoryService _categoryService;
+
         public FrmCategory()
         {
+            _categoryService = new CategoryManager(new EfCategoryDal());
             InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var categoryValues = _categoryService.TGetAll();
+            dataGridView1.DataSource = categoryValues;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Category category = new Category();
+            category.CategoryName = txtCategoryName.Text;
+            category.CategoryStatus = true;
+            _categoryService.TInsert(category);
+            MessageBox.Show("Ekleme Başarılı!");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCategoryId.Text);
+            var deletedValues = _categoryService.TGetById(id);
+            _categoryService.TDelete(deletedValues);
+            MessageBox.Show("Silme Başarılı!");
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCategoryId.Text);
+            var values = _categoryService.TGetById(id);
+            var valuesList = new List<Category> { values };
+            dataGridView1.DataSource = valuesList;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int updatedId = int.Parse(txtCategoryId.Text);
+            var updatedValues = _categoryService.TGetById(updatedId);
+            updatedValues.CategoryName = txtCategoryName.Text;
+            updatedValues.CategoryStatus = true;
+            _categoryService.TUpdate(updatedValues);
+            MessageBox.Show("Güncelleme Başarılı!");
         }
     }
 }
